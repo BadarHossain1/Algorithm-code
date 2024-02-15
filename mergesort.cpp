acca39fa5c1ec37fead594f1200296ff5ec79cef
Badar Hossain <badar12041@gmail.com>
2024-02-15 12:59:05 +0600

    Merge sort code added to the repository

---
+++ mergesort.cpp
@@ -0,0 +1,81 @@
+// write a mergesort code
+// Date: 2021/10/10
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+
+int main()
+{
+    int data[] = {20, 12, 10, 15, 2};
+    int size = sizeof(data) / sizeof(data[0]);
+    mergeSort(data, 0, size - 1);
+    printf("Sorted array in Acsending Order:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d \t", data[i]);
+    }
+    return 0;
+}
